a5dc74849d3cddff42b004770dd47f617fb71e2d
Vaibhav Srivastava <vaibhav@engagedly.com>
2021-03-22 14:44:02 +0530

    Added public folder access

--- main.js
+++ main.js
@@ -2,6 +2,7 @@ const express = require("express")
 const app = express()
 const port = 4001
 
+app.use(express.static(__dirname + '/public'));
 
 app.listen(port, () => {
   console.log('Server Started!!')
